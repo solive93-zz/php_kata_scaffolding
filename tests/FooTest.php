60bea5604054a689db5bd4e663d1cb509a811f60
Sergi Oliveres <solive@uic.es>
2020-05-16 00:04:17 +0200

    first commit

---
+++ tests/FooTest.php
@@ -0,0 +1,12 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use App\Foo;
+
+
+final class FooTest extends TestCase
+{       
+    public function test_dummy_test()
+    {
+        $this->assertTrue(true);
+    }
+}
